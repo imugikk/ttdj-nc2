56e0d244d6f44c0703e4d979a3270844b2602fe5
ugik <ugyparaptughessa@gmail.com>
2023-05-22 23:16:40 +0700

    Initial Commit

---
+++ nc2-location/nc2_locationApp.swift
@@ -0,0 +1,17 @@
+//
+//  nc2_locationApp.swift
+//  nc2-location
+//
+//  Created by Paraptughessa Premaswari on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct nc2_locationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
